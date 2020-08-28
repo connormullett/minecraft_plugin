3c763389e72b458cf079b81ffdb591f9188899d5
connor <connormullett@gmail.com>
2020-08-27 22:55:10 -0400

    added new bot command to fetch player deaths

--- death_message_bot/main.py
+++ death_message_bot/main.py
@@ -33,6 +33,32 @@ def online_players_command(update, context):
   update.message.reply_text(out)
 
 
+def all_deaths_command(update, context):
+  # build command
+  base_command = f'mcrcon -c -H 127.0.0.1 -p {os.getenv("MCRCON_PASS")}'
+  scoreboard_command = f'{base_command} "scoreboard players list"'
+
+  # get tracked names 
+  stream = os.popen(scoreboard_command)
+  # output format 'There are x tracked entities: name, name2\n'
+  response = stream.read()
+  names = response.split(':')[1].split(',') 
+
+  # query each name and format output message
+  out = ''
+  for name in names.rstrip():
+    player_death_command = f'{base_command} scoreboard players list {name}'
+    stream = os.popen(player_death_command)
+    res = stream.read().split(' ')
+    if 'deaths' in res:
+      out += f'{res[0]} has {res[-1]} death(s)\n'
+    else:
+      out += f'{res[0]} has no deaths\n'
+  
+  # send message
+  update.message.reply_text(out)
+
+
 def main():
   server_thread = threading.Thread(target=run_server)
 
@@ -45,6 +71,7 @@ def main():
   server_thread.start()
 
   dp.add_handler(CommandHandler("online", online_players_command))
+  dp.add_handler(CommandHandler("deaths", all_deaths_command))
 
   updater.start_polling()
   updater.idle()
